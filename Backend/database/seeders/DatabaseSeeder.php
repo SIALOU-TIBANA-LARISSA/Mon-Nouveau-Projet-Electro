a0850c811951aa4c3d77c82d8c3d717ea0beb71d
SIALOU <sialoutibanalarissa@gmail.com>
2026-01-30 12:25:44 +0100

    Fix seeding: use ProductSeeder only

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,     // 2. Utilisateurs (dépend des rôles)
             CategorySeeder::class, // 3. Catégories (AJOUTÉ ICI)//
             ProductSeeder::class,
-            ImportProductsFromCsvSeeder::class,
             // Les produits, commandes, etc., viendront plus tard...
         ]);
     }
